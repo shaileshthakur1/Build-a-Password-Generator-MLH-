222d9d6de32e22a085c3a66868673bd6191eb01d
Shailesh Thakur <110343920+shaileshthakur1@users.noreply.github.com>
2023-12-25 00:36:44 +0530

    added comments to clarify

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,3 +1,5 @@
+#this is program implementation of "random password generator" using random method in python.
+
 import random
 password = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz!~@#$%^&*(){}[]:'?><"
 length_password = int(input("Enter the length of the password: "))
